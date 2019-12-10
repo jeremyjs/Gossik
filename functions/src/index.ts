9521a1ee6f8e7787a11928dcf0b25cc6e364cbcd
BennoStaub <staubbe@student.ethz.ch>
2019-12-10 12:38:22 +0100

    Firebase function checks all CalendarEvents and sends a push notification to all the users devices if the event happens within 15-20min. This window is necessary to prevent multiple notifications for the same event because the function is executed every 5min.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
+import * as moment from 'moment';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,20 +10,35 @@ admin.initializeApp();
 //  response.send("Hello from Firebase!");
 // });
 
-exports.calendarEventPush = functions.pubsub.schedule('* * * * *').onRun((context) => {
+exports.calendarEventPush = functions.pubsub.schedule('*/5 * * * *').onRun((context) => {
    console.log('running');
-   let ref = admin.database().ref('/users/' + 'R1CFRqnvsmdJtxIJZIvgF1Md0lr1' + '/devices');
-    return ref.once("value", function(devices) {
-    	devices.forEach(function(device) {
-	         let payload = {
-	              notification: {
-	                  title: 'You have been invited to a trip.',
-	                  body: 'Tap here to check it out!'
-	              }
-	         };
-	         console.log('device' + device.val());
-	         admin.messaging().sendToDevice(device.val(), payload)
-	     });})
+   admin.database().ref('/users').once("value", function(users) {
+   		users.forEach(function(user) {
+   			admin.database().ref('/users/' + user.key + '/calendarEvents').once("value", function(calendarEvents) {
+   				calendarEvents.forEach(function(calendarEvent) {
+   					let eventStartTimeMiliseconds = new Date(calendarEvent.val().startTime).getTime();
+   					let eventStartTimeSeconds = eventStartTimeMiliseconds/1000;
+   					let timeNowMiliseconds = new Date().getTime();
+   					let timeNowSeconds = timeNowMiliseconds/1000;
+   					if(calendarEvent.val().active != false && eventStartTimeSeconds - timeNowSeconds < 1200 && eventStartTimeSeconds - timeNowSeconds >= 900) {
+   						let ref = admin.database().ref('/users/' + user.key + '/devices');
+					    ref.once("value", function(devices) {
+					    	devices.forEach(function(device) {
+					    		let start = moment(calendarEvent.val().startTime).format('HH:mm');
+								let end = moment(calendarEvent.val().endTime).format('HH:mm');
+						         let payload = {
+						              notification: {
+						                  title: calendarEvent.val().title,
+						                  body: start + '-' + end
+						              }
+						         };
+						         admin.messaging().sendToDevice(device.val(), payload)
+						     });})
+   					}
+   				})
+   			})
+   		})
+   });
      }
 );
 
