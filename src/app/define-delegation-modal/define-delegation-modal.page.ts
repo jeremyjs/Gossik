5fa2c0e5159dd5014ee192762ba8aeaba6ca592f
BennoStaub <staubbe@student.ethz.ch>
2019-12-15 14:58:15 +0100

    Same for DefineDelegationModalPage.

--- src/app/define-delegation-modal/define-delegation-modal.page.ts
+++ src/app/define-delegation-modal/define-delegation-modal.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavParams, ModalController } from '@ionic/angular';
+import { NavParams, ModalController, AlertController } from '@ionic/angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { TranslateService } from '@ngx-translate/core';
@@ -17,12 +17,14 @@ export class DefineDelegationModalPage implements OnInit {
 	capture = {} as Capture;
 	monthLabels = [];
 	dayLabels = [];
+  pastCheck: boolean;
 
   constructor(
   	public navParams: NavParams,
   	public modalCtrl: ModalController,
   	public translate: TranslateService,
-  	public fb: FormBuilder
+  	public fb: FormBuilder,
+    public alertCtrl: AlertController
   	) {
   }
 
@@ -69,7 +71,31 @@ export class DefineDelegationModalPage implements OnInit {
     this.modalCtrl.dismiss('cancel');
   }
 
-  addDelegation() {
+  check() {
+    if(this.defineDelegationForm.value.deadline) {
+      if(new Date(this.defineDelegationForm.value.deadline) < new Date() && !this.pastCheck) {
+        this.translate.get(["The selected date lies in the past. Please check if that is wanted.", "Ok"]).subscribe( alertMessage => {
+          this.alertCtrl.create({
+              message: alertMessage["The selected date lies in the past. Please check if that is wanted."],
+              buttons: [
+                      {
+                          text: alertMessage["Ok"]
+                        }
+                    ]
+          }).then ( alert => {
+            alert.present();
+            this.pastCheck = true
+          });
+        });
+      } else {
+        this.save();
+      }
+    } else {
+      this.save();
+    }
+  }
+
+  save() {
   	this.modalCtrl.dismiss(this.defineDelegationForm.value)
   }
 
