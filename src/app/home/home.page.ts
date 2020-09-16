213b529be4d62e50a2315adee77d7af2d63b7b76
BennoStaub <benno.staub@hotmail.com>
2020-09-16 22:29:52 +0200

    Started working on the computeDynamicPriority() function. 3 influence factors are computed already.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2613,11 +2613,13 @@ export class HomePage {
     }
 
   	showDoableActions() {
-		  this.skippedAllToDos = false;
+		this.skippedAllToDos = false;
 		let duration: number = 100000;
 		if(this.duration > 0) {
 			duration = this.duration;
 		}
+		console.log('duration ' + String(duration));
+		console.log('this.duration ' + String(this.duration));
 		this.doableActionArray = [];
 		for(let action of this.actionArray) {
 			if(action.active != false) {
@@ -2633,7 +2635,7 @@ export class HomePage {
 						attributeCheck = false;
 					}
 				}
-				if(action.time/1 <= this.duration/1 && !action.taken && (this.goalKeyArray.indexOf(action.goalid) != -1 || this.goalKeyArray.length == 0 ) && attributeCheck) {
+				if(action.time/1 <= duration/1 && !action.taken && (this.goalKeyArray.indexOf(action.goalid) != -1 || this.goalKeyArray.length == 0 ) && attributeCheck) {
 				this.doableActionArray.push(action);
 				}
 			}
@@ -2644,7 +2646,48 @@ export class HomePage {
 				this.presentToast(translation["There is no doable action for that time."]);
 			})
 		}
-  	}
+		this.computeDynamicPriority(this.doableActionArray[0]);
+	}
+
+	computeDynamicPriority(action: Action) {
+		let priorityInfluenceDeadlie: number = 0;
+		if(action.deadline) {
+			let daysUntilDeadline = Math.round((new Date(action.deadline).getTime() - new Date().getTime())/(24*3600*1000));
+			if(daysUntilDeadline < 5) {
+				priorityInfluenceDeadlie = (5-daysUntilDeadline)*10;
+			}
+		}
+		let priorityInfluenceProcastination: number = 0;
+		if(action.createDate) {
+			let daysSinceCreated: number = Math.round((new Date().getTime() - new Date(action.createDate).getTime())/(24*3600*1000));
+			priorityInfluenceProcastination = daysSinceCreated / 6;
+		}
+		let priorityInfluenceSchedule: number = 0;
+		let learnedSchedule = JSON.parse(this.userProfile['learnedSchedule'].toString());
+		let localeDate = new Date(new Date().getTime() - Number(this.userProfile.timezoneOffset*60*1000));
+		let weekDay = localeDate.getDay() - 1;
+		if(weekDay == -1) {
+			weekDay = 6;
+		}
+		//getHours() gives locale hours already, so no need to use localeDate
+		let hour = new Date().getHours();
+		let learnedScheduleHour = weekDay * 24 + hour;
+		let max: number = 0;
+		let maxKey = undefined;
+		let scoreDict:any = {};
+		for(let projectid in learnedSchedule[learnedScheduleHour]) {
+			if(learnedSchedule[learnedScheduleHour][projectid] > max) {
+				max = learnedSchedule[learnedScheduleHour][projectid];
+				maxKey = projectid;
+			}
+			if(learnedSchedule[learnedScheduleHour][projectid] > 0) {
+				scoreDict[projectid] = learnedSchedule[learnedScheduleHour][projectid];
+			}
+		}
+		if(max > 0 && scoreDict[action.goalid]) {
+			priorityInfluenceSchedule = scoreDict[action.goalid] / max * 20;
+		}
+	}
 
   	skipAction() {
   		this.doableActionArray.splice(0, 1);
