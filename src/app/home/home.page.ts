67f682a791d79ef26c93a38e347fdba19233b188
BennoStaub <benno.staub@hotmail.com>
2020-10-12 15:09:01 +0200

    Done table meetings & appointments and critical tasks on Assistant Page. But no critical tasks computation yet.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -856,50 +856,6 @@ export class HomePage {
 		if(this.userProfile && !this.userProfile['assistant']) {
 			this.db.initiateAssistant(this.auth.userid);
 		}
-		this.showTutorial('assistant');
-		let learnedSchedule = JSON.parse(this.userProfile['learnedSchedule'].toString());
-		this.calendar.currentDate = new Date();
-		let learnedScheduleDate = new Date();
-		let weekDay = learnedScheduleDate.getDay();
-		let monday: Date;
-		if(weekDay == 0) {
-			monday = new Date(learnedScheduleDate.getTime() - 6*24*3600*1000);
-		} else {
-			monday = new Date(learnedScheduleDate.getTime() - (weekDay-1)*24*3600*1000);
-		}
-		monday.setHours(0,0,0);
-		this.eventSource = [];
-		for(let hour in learnedSchedule) {
-			let max: number = 0;
-			let maxKey = undefined;
-			let sum: number = 0;
-			for(let projectid in learnedSchedule[hour]) {
-				if(learnedSchedule[hour][projectid] > 0) {
-					sum += learnedSchedule[hour][projectid];
-				}
-				if(learnedSchedule[hour][projectid] > max) {
-					max = learnedSchedule[hour][projectid];
-					maxKey = projectid;
-				}
-			}
-			if(maxKey && maxKey != 'unassigned') {
-				let learnedScheduleEvent = {
-					startTime: monday,
-					endTime: new Date(monday.getTime() + 3600*1000),
-					title: this.goalDict[maxKey].name,
-					allDay: false,
-					color: this.goalDict[maxKey].color,
-					opacity: max / sum
-				}
-				this.eventSource.push(learnedScheduleEvent);
-			}
-			monday = new Date(monday.getTime() + 3600*1000);
-		}
-		let events = this.eventSource;
-		this.eventSource = [];
-		setTimeout(() => {
-			this.eventSource = events;
-		});
 		this.pageTitle = "Assistant";
 		this.changePage('AssistantPage');
     }
