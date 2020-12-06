6c76836c4b6563585a479d7fc4b86a9de875427e
BennoStaub <benno.staub@hotmail.com>
2020-12-06 15:05:48 +0100

    Most of the tutorial has now a back button.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -900,33 +900,70 @@ export class HomePage {
 				if(params[3] == 0) {
 					this.presentPopover('add', {tutorial: true});
 				} else if(params[3] == 1) {
-					let text = "On the top, you can enter your available time. For example, when you have 20min available, I will only show you to-dos that are doable within 20min.";
-					let buttons = ["Next"];
-					let title = '(4/8)';
-					this.presentPopover('showInteraction', [text, buttons, title, 2]);
+					if(data.data == 'Next') {
+						let text = "On the top, you can enter your available time. For example, when you have 20min available, I will only show you to-dos that are doable within 20min.";
+						let buttons = ["Back", "Next"];
+						let title = '(4/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 2]);
+					}
 				} else if(params[3] == 2) {
-					let text = "The smart assistant will prioritize your to-dos for you and show your the most relevant tasks according to your current situation.";
-					let buttons = ["Next"];
-					let title = '(5/8)';
-					this.presentPopover('showInteraction', [text, buttons, title, 3]);
+					if(data.data == 'Next') {
+						let text = "The smart assistant will prioritize your to-dos for you and show your the most relevant tasks according to your current situation.";
+						let buttons = ["Back", "Next"];
+						let title = '(5/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 3]);
+					} else if(data.data == 'Back') {
+						let text = "Here on the Do page, you can work off your to-dos.";
+						let buttons = ["Next"];
+						let title = '(3/8)';
+						this.presentPopover('showInteraction', [text, buttons, title,1]);
+					}
 				} else if(params[3] == 3) {
 					this.goToAssistantPage();
-					let text = "Here on the Assistant page, I show you statistics and facts that I learned and observed.";
-					let buttons = ["Next"];
-					let title = '(6/8)';
-					this.presentPopover('showInteraction', [text, buttons, title, 4]);
+					if(data.data == 'Next') {
+						let text = "Here on the Assistant page, I show you statistics and facts that I learned and observed.";
+						let buttons = ["Back", "Next"];
+						let title = '(6/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 4]);
+					} else if(data.data == 'Back') {
+						let text = "On the top, you can enter your available time. For example, when you have 20min available, I will only show you to-dos that are doable within 20min.";
+						let buttons = ["Back", "Next"];
+						let title = '(4/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 2]);
+					}
 				} else if(params[3] == 4) {
-					let text = "You can also change these things to modify how I prioritize and schedule your to-dos for you.";
-					let buttons = ["Next"];
-					let title = '(7/8)';
-					this.presentPopover('showInteraction', [text, buttons, title, 5]);
+					if(data.data == 'Next') {
+						let text = "You can also change these things to modify how I prioritize and schedule your to-dos for you.";
+						let buttons = ["Back", "Next"];
+						let title = '(7/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 5]);
+					} else if(data.data == 'Back') {
+						let text = "The smart assistant will prioritize your to-dos for you and show your the most relevant tasks according to your current situation.";
+						let buttons = ["Back", "Next"];
+						let title = '(5/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 3]);
+					}
 				} else if(params[3] == 5) {
-					let text = "And I give you suggestions that should help you to get everything under control. For example, when a deadline approaches, I will suggest you to increase the priority of this task to avoid stress.";
-					let buttons = ["Close"];
-					let title = '(8/8)';
-					this.presentPopover('showInteraction', [text, buttons, title, 6]);
+					if(data.data == 'Next') {
+						let text = "And I give you suggestions that should help you to get everything under control. For example, when a deadline approaches, I will suggest you to increase the priority of this task to avoid stress.";
+						let buttons = ["Back", "Close"];
+						let title = '(8/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 6]);
+					} else if(data.data == 'Back') {
+						let text = "Here on the Assistant page, I show you statistics and facts that I learned and observed.";
+						let buttons = ["Back", "Next"];
+						let title = '(6/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 4]);
+					}
 				} else if(params[3] == 6) {
-					this.db.finishTutorial(this.auth.userid);
+					if(data.data == 'Close') {
+						this.db.finishTutorial(this.auth.userid);
+					} else if(data.data == 'Back') {
+						let text = "You can also change these things to modify how I prioritize and schedule your to-dos for you.";
+						let buttons = ["Back", "Next"];
+						let title = '(7/8)';
+						this.presentPopover('showInteraction', [text, buttons, title, 5]);
+					}
 				}
 			});
 		}
