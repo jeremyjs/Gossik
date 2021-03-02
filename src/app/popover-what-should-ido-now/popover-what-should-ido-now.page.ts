cce6cf2fa8257770bf2cfe966c9fbaaeae76f303
BennoStaub <benno.staub@hotmail.com>
2021-03-02 16:10:33 +0100

    'what should I dow now' button implemented done, works. But needs to be better designed.

--- src/app/popover-what-should-ido-now/popover-what-should-ido-now.page.ts
+++ src/app/popover-what-should-ido-now/popover-what-should-ido-now.page.ts
@@ -14,6 +14,10 @@ export class PopoverWhatShouldIDoNowPage implements OnInit {
   goalDict: {} = {};
   priorities: any[] = [];
   userProfile: any;
+  priorityInfluenceFactorDeadlineText: string;
+  priorityInfluenceFactorProcrastinationText: string;
+  priorityInfluenceFactorLearnedScheduleText: string;
+  priorityInfluenceFactorFocusText: string;
 
   constructor(
     public translate: TranslateService,
@@ -27,6 +31,7 @@ export class PopoverWhatShouldIDoNowPage implements OnInit {
     this.priorities = this.navParams.get('priorities');
     this.userProfile = this.navParams.get('userProfile');
     this.doableActionArray.sort((a, b) => (this.computeDynamicPriority(a) < this.computeDynamicPriority(b)) ? 1 : -1);
+    this.computeDynamicPriority(this.doableActionArray[0], true);
   }
 
   dateFormated(date) {
@@ -36,6 +41,7 @@ export class PopoverWhatShouldIDoNowPage implements OnInit {
   showNext() {
     if(this.doableActionArray.length > 1) {
       this.doableActionArray.shift();
+      this.computeDynamicPriority(this.doableActionArray[0], true);
     }
   }
 
@@ -43,22 +49,40 @@ export class PopoverWhatShouldIDoNowPage implements OnInit {
     this.popoverCtrl.dismiss(todo);
   }
 
-  computeDynamicPriority(action: Action): number {
-		let priorityInfluenceDeadlie: number = 0;
+  computePriorityInfluenceFactorDeadline(action: Action, text: boolean): number {
+    let priorityInfluenceFactorDeadline: number = 0;
 		let daysUntilDeadline: number;
 		if(action.deadline) {
 			daysUntilDeadline = Math.round((new Date(action.deadline).getTime() - new Date().getTime())/(24*3600*1000));
 			if(daysUntilDeadline < 5) {
-				priorityInfluenceDeadlie = (5-daysUntilDeadline)*10;
+        priorityInfluenceFactorDeadline = (5 - daysUntilDeadline)*10;
+        if(text) {
+          this.priorityInfluenceFactorDeadlineText = "The deadline is approaching soon";
+        }
 			}
 		}
-		let priorityInfluenceProcastination: number = 0;
+    return priorityInfluenceFactorDeadline;
+  }
+
+  computePriorityInfluenceFactorProcrastination(action: Action, text: boolean): number {
+    let priorityInfluenceFactorProcrastination: number = 0;
 		let daysSinceCreated: number;
 		if(action.createDate) {
 			daysSinceCreated = Math.round((new Date().getTime() - new Date(action.createDate).getTime())/(24*3600*1000));
-			priorityInfluenceProcastination = daysSinceCreated / 6;
-		}
-		let priorityInfluenceSchedule: number = 0;
+      priorityInfluenceFactorProcrastination = daysSinceCreated / 6;
+      if(text) {
+        if(priorityInfluenceFactorProcrastination > 2) {
+          this.priorityInfluenceFactorProcrastinationText = "It has been on your to-do list for a long time";
+        } else if(priorityInfluenceFactorProcrastination > 5) {
+          this.priorityInfluenceFactorProcrastinationText = "It has been on your to-do list for way too long";
+        }
+      }
+    }
+    return priorityInfluenceFactorProcrastination;
+  }
+
+  computePriorityInfluenceFactorLearnedSchedule(action: Action, text: boolean): number {
+    let priorityInfluenceFactorLearnedSchedule: number = 0;
 		let learnedSchedule = JSON.parse(this.userProfile['learnedSchedule'].toString());
 		let localeDate = new Date(new Date().getTime() - Number(this.userProfile.timezoneOffset*60*1000));
 		let weekDay = localeDate.getDay() - 1;
@@ -79,17 +103,43 @@ export class PopoverWhatShouldIDoNowPage implements OnInit {
 			}
 		}
 		if(max > 0 && scoreDict[action.goalid]) {
-			priorityInfluenceSchedule = scoreDict[action.goalid] / max * 20;
-		}
-		let priorityInfluencePriority: number = 10 * action.priority;
-		// compute influence factor based on focus
-		let priorityInfluenceFocus: number = 0;
+      priorityInfluenceFactorLearnedSchedule = scoreDict[action.goalid] / max * 20;
+      if(text) {
+        this.priorityInfluenceFactorLearnedScheduleText = "At this time, you usually work on this project";
+      }
+    }
+    return priorityInfluenceFactorLearnedSchedule;
+  }
+
+  computePriorityInfluenceFactorFocus(action: Action, text: boolean): number {
+    let priorityInfluenceFactorFocus: number = 0;
 		for(let key in this.userProfile.focusProjects) {
 			if(this.userProfile.focusProjects[key] == action.goalid) {
-				priorityInfluenceFocus = 15;
+        priorityInfluenceFactorFocus = 15;
+        if(text) {
+          this.priorityInfluenceFactorFocusText = "You set the focus to this project";
+        }
 			}
-		}
-		return priorityInfluenceDeadlie + priorityInfluencePriority + priorityInfluenceProcastination + priorityInfluenceSchedule + priorityInfluenceFocus;
+    }
+    return priorityInfluenceFactorFocus;
+  }
+
+  computePriorityInfluenceFactorPriority(action: Action): number {
+    let priorityInfluenceFactorPriority: number = 10 * action.priority;
+    return priorityInfluenceFactorPriority
+  }
+
+  computeDynamicPriority(action: Action, text: boolean = undefined): number {
+    this.priorityInfluenceFactorDeadlineText = undefined;
+    this.priorityInfluenceFactorProcrastinationText = undefined;
+    this.priorityInfluenceFactorLearnedScheduleText = undefined;
+    this.priorityInfluenceFactorFocusText = undefined;
+		let priorityInfluenceFactorDeadline = this.computePriorityInfluenceFactorDeadline(action, text);
+		let priorityInfluenceFactorProcrastination = this.computePriorityInfluenceFactorProcrastination(action, text);
+    let priorityInfluenceFactorLearnedSchedule = this.computePriorityInfluenceFactorLearnedSchedule(action, text);
+    let priorityInfluenceFactorFocus = this.computePriorityInfluenceFactorFocus(action, text);
+		let priorityInfluenceFactorPriority = this.computePriorityInfluenceFactorPriority(action);
+		return priorityInfluenceFactorDeadline + priorityInfluenceFactorProcrastination + priorityInfluenceFactorLearnedSchedule + priorityInfluenceFactorFocus + priorityInfluenceFactorPriority;
 	}
 
 }
